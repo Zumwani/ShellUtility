d570d5361c1a5a9ed0b2b29aebddcd6d05b9020b
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2021-05-15 00:09:03 +0200

    Fixed memory leak in IconUtility.

--- ShellUtility.NotifyIcons/Utility/IconUtility.cs
+++ ShellUtility.NotifyIcons/Utility/IconUtility.cs
@@ -10,20 +10,46 @@ namespace ShellUtility.NotifyIcons
     internal static class IconUtility
     {
 
-        [DllImport("shell32.dll")]
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
         static extern IntPtr ExtractAssociatedIcon(IntPtr hInst, string lpIconPath, out ushort lpiIcon);
+
+        [DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
 
         public static BitmapSource IconFromResourceHandle(IntPtr handle, string path)
         {
 
-            var extractedHandle = ExtractAssociatedIcon(handle, path, out var result);
-            if (result != 0)
-                return null;
+            try
+            {
 
-            var img = Imaging.CreateBitmapSourceFromHIcon(extractedHandle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-            img.Freeze();
+                if (handle == IntPtr.Zero)
+                    return ExtractIcon();
 
-            return img;
+                var icon = Imaging.CreateBitmapSourceFromHIcon(handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                icon.Freeze();
+                _ = DeleteObject(handle);
+                return icon;
+
+            }
+            catch (COMException)
+            {
+                return ExtractIcon();
+            }
+
+            BitmapSource ExtractIcon()
+            {
+
+                var extractedHandle = ExtractAssociatedIcon(handle, path, out var result);
+                if (result != 0)
+                    return null;
+
+                var img = Imaging.CreateBitmapSourceFromHIcon(extractedHandle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                img.Freeze();
+                _ = DeleteObject(extractedHandle);
+
+                return img;
+
+            }
 
         }
 
